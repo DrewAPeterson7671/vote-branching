9b3970b62829369f2bc896c9b4d08488931f443f
Student <example@example.com>
2019-10-24 10:20:43 -0700

    Add functionality to number input and handle overlapping usages.

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,9 @@ $(document).ready(function(){
     var age = $("input#age").val();
     if (age > 18) {
       $('.register').show();
+      $('.info').hide();
     } else if (age <= 18) {
+      $('.register').hide();
       $('.info').show();
     } else {
       console.log('Error')
